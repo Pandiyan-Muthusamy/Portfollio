8d30b615c0b7ca0881f549d2316c9010843d5325
Pandiyan-Muthusamy <developer.pandiyan@gmail.com>
2025-03-09 00:59:41 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,7 +22,7 @@ export default function Sidebar({ setCurrentPage, currentPage }) {
   return (
     <>
       <div
-        className={`fixed left-0 h-full w-64 bg-[#d2f2c9] text-black transform z-50 ${
+        className={`fixed left-0 h-full w-64 bg-red text-black transform z-50 ${
           isOpen ? "translate-x-0" : "-translate-x-64"
         } md:translate-x-0 transition-transform duration-300 ease-in-out `}
       >

efabbd3e33a8cccde9b759f5788deb3f459a60e1
Pandiyan-Muthusamy <developer.pandiyan@gmail.com>
2025-03-10 03:11:23 +0530

    First Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ export default function Home({ setCurrentPage }) {
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900"
+        className="text-2xl md:text-5xl font-bold text-gray-900"
       >
         {portfolioData.name}
       </motion.h1>
